71f55d32f417b2de5ff67fb9a3c6aefee1552370
Jathmel Gordon <jathmel@gmail.com>
2018-05-02 22:28:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ end
 
 def summon_captain_planet(elements)# code an argument here
   # Your code here
-  elements
+  elements.collect do |el|
+    el.capitalize
+    el << '!'
+  end
 end
 
 def long_planeteer_calls# code an argument here
