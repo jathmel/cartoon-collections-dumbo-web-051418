5547ac90ebd6a619767e6699b9d9ff04f43c2a3c
Jathmel Gordon <jathmel@gmail.com>
2018-05-02 22:39:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.each do |food|
+  foods.each do |food|
     if cheese_types.include?(food)
       return food
     end
